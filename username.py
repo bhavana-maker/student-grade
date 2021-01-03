9a4c5e5426188175cb2bc07608331c6c4e1a0a09
bhavana-maker <76758269+bhavana-maker@users.noreply.github.com>
2021-01-03 15:21:29 +0530

    Add files via upload

---
+++ username.py
@@ -0,0 +1,15 @@
+print("enter correct user name and password combo to continue")
+count=0
+username=bhavanagadde
+password=bhavana#1995
+while password!="bhavana#1995" and username!="bhavanagadde" and count<3:
+ username=input('enter username=') and password=input('enter password=')
+if username==bhavanagadde and password==bhavana#1995:
+ print("access granted")
+elif:
+ print("access denied.try again")
+ count-=1
+else:
+ print("after 3 attempts denied")
+    
+    
